178439d6e6ddf172868891784eb1a3d1acb389d8
Andrey Shamis <lolnik@gmail.com>
2013-04-20 00:06:13 +0300

    Some packets - not used
    Signed-off-by: Andrey Shamis <lolnik@gmail.com>

---
+++ Visualisator/Visualisator/Packets/TDLSDiscovery.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Reflection;
+
+namespace Visualisator.Packets
+{
+    [Serializable()]
+    class TDLSDiscovery : SimulatorPacket, IPacket, ISerializable
+    {
+  
+        public TDLSDiscovery(SimulatorPacket pack)
+        {
+            Type t = typeof(SimulatorPacket);
+            PropertyInfo[] properties = t.GetProperties();
+            foreach (PropertyInfo pi in properties)
+            {
+                pi.SetValue(this, pi.GetValue(pack, null), null);
+            }
+        }
+
+        public TDLSDiscovery(Packets.Data pack)
+        {
+            Type t = typeof(Data);
+            PropertyInfo[] properties = t.GetProperties();
+            foreach (PropertyInfo pi in properties)
+            {
+                pi.SetValue(this, pi.GetValue(pack, null), null);
+            }
+        }
+    }
+}
